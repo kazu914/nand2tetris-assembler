bdf62976aa7bf0d45453456e1aae989c8ef42857
Kazuaki Nomura <nkz0914ssk@gmail.com>
2020-11-03 12:32:15 +0900

    made assembler struct

--- src/main.rs
+++ src/main.rs
@@ -13,24 +13,39 @@ fn read_lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
     buf.lines().map(|l| l.expect("Failed to read")).collect()
 }
 
-fn main() {
-    let lines = read_lines_from_file("./Add.asm");
-    let mut parser = parser::Parser::new(lines);
-    while parser.has_more_commands {
-        parser.advance();
-        match &parser.command_type {
-            None => continue,
-            Some(parser::CommandType::Ccommand) => {
-                let dest = code::dest_to_binary(&parser.dest);
-                let comp = code::comp_to_binary(&parser.comp);
-                let jump = code::jump_to_binary(&parser.jump);
-                println!("111{}{}{}", comp, dest, jump);
+pub struct Assembler {
+    parser: parser::Parser,
+}
+
+impl Assembler {
+    pub fn new(lines: Vec<String>) -> Assembler {
+        let parser = parser::Parser::new(lines);
+        Assembler { parser }
+    }
+
+    fn parse(&mut self) {
+        while self.parser.has_more_commands {
+            self.parser.advance();
+            match &self.parser.command_type {
+                None => continue,
+                Some(parser::CommandType::Ccommand) => {
+                    let dest = code::dest_to_binary(&self.parser.dest);
+                    let comp = code::comp_to_binary(&self.parser.comp);
+                    let jump = code::jump_to_binary(&self.parser.jump);
+                    println!("111{}{}{}", comp, dest, jump);
+                }
+                Some(parser::CommandType::Acommand) => {
+                    let symbol = code::decimal_to_binary(&self.parser.symbol);
+                    println!("{}", symbol);
+                }
+                Some(parser::CommandType::Lcommand) => println!("pass:L"),
             }
-            Some(parser::CommandType::Acommand) => {
-                let symbol = code::decimal_to_binary(&parser.symbol);
-                println!("{}", symbol);
-            }
-            Some(parser::CommandType::Lcommand) => println!("pass:L"),
         }
     }
+}
+
+fn main() {
+    let lines = read_lines_from_file("./Add.asm");
+    let mut assembler = Assembler::new(lines);
+    assembler.parse()
 }
